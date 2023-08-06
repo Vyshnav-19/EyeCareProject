208bf332115f935f288fca2055540bbcc5864fa9
Vyshnav-19 <133677155+Vyshnav-19@users.noreply.github.com>
2023-08-07 00:12:32 +0530

    AfterHosp&Opt_auth.py

--- website/auth.py
+++ website/auth.py
@@ -70,32 +70,116 @@ def signup():
     return jsonify({'success': True, 'message': 'Signup successful', 'userid': str(result.inserted_id)}), 201
 
 
-@auth.route('/profile/update', methods=['POST'])
-def update_profile(logged_in_user_email):
-    data = request.get_json()
-    new_profile_data = data['profile_data']
+#@auth.route('/profile/update', methods=['POST'])
+#def update_profile(logged_in_user_email):
+  #  data = request.get_json()
+ #   new_profile_data = data['profile_data']
 
     # Get the logged-in user's email from the authentication token or session
     # Assuming you have implemented the authentication mechanism
 
     # Find the user in the database based on the email
-    user = users_collection.find_one({'email': logged_in_user_email})
+#    user = users_collection.find_one({'email': logged_in_user_email})
 
-    if user:
+  #  if user:
         # Update the user's profile data
-        user['full_name'] = new_profile_data.get('full_name', user['full_name'])
-        user['email'] = new_profile_data.get('email', user['email'])
-        user['age'] = new_profile_data.get('age', user['age'])
-        user['gender'] = new_profile_data.get('gender', user['gender'])
-        user['password'] = new_profile_data.get('password', user['password'])
+  #      user['full_name'] = new_profile_data.get('full_name', user['full_name'])
+    #     user['email'] = new_profile_data.get('email', user['email'])
+      #   user['age'] = new_profile_data.get('age', user['age'])
+        # user['gender'] = new_profile_data.get('gender', user['gender'])
+         #user['password'] = new_profile_data.get('password', user['password'])
 
         # Save the updated user profile to the database
 
         # Return success response
-        return jsonify({'success': True, 'message': 'Profile updated successfully'}), 200
-    else:
+  #       return jsonify({'success': True, 'message': 'Profile updated successfully'}), 200
+ #    else:
         # Return error response
-        return jsonify({'success': False, 'message': 'User not found'}), 404
+ #        return jsonify({'success': False, 'message': 'User not found'}), 404
+
+
+# Set the folder path to save the uploaded images
+UPLOAD_FOLDER = 'upldfldr'  # Replace with your desired upload directory path
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+# Function to check if the file extension is allowed
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in {'jpg', 'jpeg', 'png'}
+
+
+@auth.route('/detect-cataract', methods=['POST'])
+def detect_cataract_route():
+    # Check if the 'image' file is present in the request
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file found'})
+
+    image_file = request.files['image']
+
+    # Check if the file has a valid filename and extension
+    if image_file.filename == '':
+        return jsonify({'error': 'Empty filename'})
+
+    if not allowed_file(image_file.filename):
+        return jsonify({'error': 'Invalid file extension'})
+
+    # Save the uploaded image to the upload folder
+    # filename = secure_filename(image_file.filename)
+    # print(filename)
+    # file_path = os.path.join(UPLOAD_FOLDER, filename)
+    image_file.save("website/upldfldr/image.jpg")
+
+    # Perform cataract detection on the uploaded image
+    result = predict("website/upldfldr/image.jpg")
+
+    return jsonify({'result': result})
+
+
+@auth.route('/hospital_details', methods=['GET'])
+def get_hospital_details():
+    place = request.args.get('Place')
+
+    # Query the database to find hospitals with the given place
+    hospitals = hosp_collection.find({"Place": place})
+
+    # Prepare the response as a list of dictionaries
+    hospital_list = []
+    for hospital in hospitals:
+        hospital_details = {
+            "Hospital_name": hospital["Hospital_Name"],
+            "Address": hospital["Address"],
+            "Place": hospital["Place"],
+            "PhoneNo": hospital["PhoneNo"]
+        }
+        hospital_list.append(hospital_details)
+
+    return jsonify(hospital_list)
+
+
+@auth.route('/optical_details', methods=['GET'])
+def get_optical_details():
+    place = request.args.get('Place')
+
+    optical = opt_collection.find({"Place": place})
+
+    opticals_list = []
+    for optical in optical:
+        optical_details = {
+            "Opticals_Name": optical["Opticals_Name"],
+            "Address": optical["Address"],
+            "Place": optical["Place"],
+            "PhoneNo": optical["PhoneNo"]
+        }
+        opticals_list.append(optical_details)
+
+    return jsonify(opticals_list)
+
+
+#@auth.route('/Health_record',method=['GET'])
+#@login_required
+#def get_health_record():
 
 
 if __name__ == '__main__':
